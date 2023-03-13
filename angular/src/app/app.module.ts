bf9bcd6d7219e880dc1dea5474f6de71fe50f236
Thomas Munzer <thomas.munzer@gmail.com>
2023-03-13 19:45:57 +0100

    add progress bar

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -13,6 +13,7 @@ import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatTableModule } from '@angular/material/table';
 import { MatTooltipModule } from '@angular/material/tooltip';
 
@@ -33,6 +34,7 @@ import { MatTooltipModule } from '@angular/material/tooltip';
     MatIconModule,
     MatInputModule,
     MatListModule,
+    MatProgressBarModule,
     MatTableModule,
     MatTooltipModule
   ],
